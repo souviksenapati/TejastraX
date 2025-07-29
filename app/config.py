67ec995cde147a4d2745a1665b298e36e7d3bc07
Souvik Senapati <souviksenapati85@gmail.com>
2025-07-29 22:07:11 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL = "models/gemini-2.0-flash-exp"
+GEMINI_MODEL = "models/gemini-2.0-flash"
 
 EMBED_MODEL = "Gemini"
 EMBED_DIM = 768  # As returned by Gemini
